0334f03051f8dcc1a78de3936ff76eab09c97c0a
aspirina765 <molinalucas765@gmail.com>
2022-08-07 17:48:18 -0400

    refact

--- start-docker-images.sh
+++ start-docker-images.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/bash env
+export dockerhub_repo=ratoloko765
+
+echo 'export dockerhub_repo=ratoloko765' >> ~/.bashrc 
+source ~/.bashrc
+
+docker-compose up -d
+docker images 
+docker tag flask-boilerplate_app:latest ${dockerhub_repo}/flask-boilerplate_app:latest
+docker login
+docker push ${dockerhub_repo}/flask-boilerplate_app:latest
 docker-compose up -d
 docker images 
 docker tag flask-boilerplate_app:latest ratoloko765/flask-boilerplate_app:latest
